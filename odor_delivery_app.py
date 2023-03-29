6048d7546a4578093e8a88d370e7fba0518387ad
Jane Huang <62308312+janeswh@users.noreply.github.com>
2023-03-29 12:03:30 -0400

    reorganized parameter saving

--- odor_delivery_app.py
+++ odor_delivery_app.py
@@ -28,6 +28,8 @@ def initialize_states():
         st.session_state.acq_params = False
     if "settings_saved" not in st.session_state:
         st.session_state.settings_saved = False
+    if "settings_changed" not in st.session_state:
+        st.session_state.settings_changed = False
     if "file_name" not in st.session_state:
         st.session_state.file_name = False
 
@@ -46,26 +48,20 @@ def clear_settings_fields():
     st.session_state["default_time_btw_odors"] = 10
 
 
-class AcqParams:
+def get_setting_inputs():
     """
-    Defines the class for holding GUI acquisition parameters
+    Creates input fields for experiment settings and saves to AcqParams object
     """
-
-    pass
-
-
-def main():
-    set_webapp_params()
-    initialize_states()
-
     st.header("Session Info")
+    st.session_state.settings_saved = False
+    mouse_id = False  # Set to false before user input
 
-    tab1_sessioninfo_col1, tab1_sessioninfo_col2 = st.columns(2)
-    with tab1_sessioninfo_col1:
+    sessioninfo_col1, sessioninfo_col2 = st.columns(2)
+    with sessioninfo_col1:
         mouse_id = st.text_input(
             label="Mouse ID", placeholder="123456-1-2", key="text"
         )
-    with tab1_sessioninfo_col2:
+    with sessioninfo_col2:
         roi = st.number_input(label="ROI #", min_value=1, key="default_roi")
 
     st.header("Experiment Settings")
@@ -80,7 +76,7 @@ def main():
         num_odors = st.selectbox(
             label="Number of Odors",
             options=range(1, 9),
-            index=6,
+            index=7,
             key="default_num_odors",
         )
 
@@ -107,16 +103,73 @@ def main():
             key="default_time_btw_odors",
         )
 
-    # If any values were changed, prompt to save settings again
-    if None not in (
+    now_date = datetime.datetime.now()
+
+    return (
         mouse_id,
         roi,
         num_odors,
         num_trials,
         odor_duration,
         time_btw_odors,
-    ):
-        st.session_state.settings_saved = False
+        now_date,
+    )
+
+
+def save_settings(settings_dict):
+    """
+    Saves experiment settings to AcqParams object
+    """
+    st.session_state.settings_saved = True
+
+    saved_acq_settings = AcqParams(
+        settings_dict["date"],
+        settings_dict["mouse"],
+        settings_dict["roi"],
+        settings_dict["num_odors"],
+        settings_dict["num_trials"],
+        settings_dict["odor_duration"],
+        settings_dict["time_btw_odors"],
+    )
+
+    st.session_state.file_name = (
+        f"{st.session_state.acq_params['date']}_"
+        f"{st.session_state.acq_params['mouse']}_"
+        f"ROI{st.session_state.acq_params['roi']}_Odor"
+    )
+
+    st.info(f"Settings saved for {st.session_state.file_name}.")
+
+
+def make_settings_fields():
+    """
+    Creates layout for experiment settings
+    """
+    (
+        mouse_id,
+        roi,
+        num_odors,
+        num_trials,
+        odor_duration,
+        time_btw_odors,
+        now_date,
+    ) = get_setting_inputs()
+
+    st.session_state.acq_params = {
+        "mouse": mouse_id,
+        "roi": roi,
+        "date": now_date.strftime("%y%m%d"),
+        "num_odors": num_odors,
+        "num_trials": num_trials,
+        "odor_duration": odor_duration,
+        "time_btw_odors": time_btw_odors,
+    }
+
+    # If any values were changed, prompt to save settings again
+    for v in st.session_state.acq_params.values():
+        if v is not None:
+            # st.session_state.settings_saved = False
+            st.session_state.settings_changed = True
 
     buttons_col1, buttons_col2 = st.columns([0.25, 1])
 
@@ -127,36 +180,56 @@ def main():
         st.session_state.acq_params = False
         st.session_state.file_name = False
 
-    if mouse_id:  # Show save button if mouse_id isn't empty
+    if mouse_id:
         with buttons_col2:
             save_params = st.button("Save Settings")
 
-        if save_params:  # Save settings to session state dict
-            st.session_state.settings_saved = True
-            now_date = datetime.datetime.now()
-            st.session_state.acq_params = {
-                "mouse": mouse_id,
-                "roi": roi,
-                "date": now_date.strftime("%y%m%d"),
-                "num_odors": num_odors,
-                "num_trials": num_trials,
-                "odor_duration": odor_duration,
-                "time_btw_odors": time_btw_odors,
-            }
+        if save_params:  # Save dict to AcqParams object
+            save_settings(st.session_state.acq_params)
 
-            st.session_state.file_name = (
-                f"{st.session_state.acq_params['date']}_"
-                f"{st.session_state.acq_params['mouse']}_"
-                f"ROI{st.session_state.acq_params['roi']}_Odor"
-            )
 
-            st.info(f"Settings saved for {st.session_state.file_name}.")
+class AcqParams:
+    """
+    Defines the class for holding GUI acquisition parameters
+    """
+
+    def __init__(
+        self,
+        date,
+        mouse_id,
+        roi,
+        num_odors,
+        num_trials,
+        odor_duration,
+        time_btw_odors,
+    ):
+        self.date = date
+        self.mouse_id = mouse_id
+        self.roi = roi
+        self.num_odors = num_odors
+        self.num_trials = num_trials
+        self.odor_duration = odor_duration
+        self.time_btw_odors = time_btw_odors
+
+
+def main():
+    set_webapp_params()
+    initialize_states()
+
+    make_settings_fields()
+    # pdb.set_trace()
 
     if st.session_state.settings_saved:
         exp_type = st.radio(
             label="Experiment type",
-            options=("DIY", "Random Trials", "Single Trial"),
+            options=(
+                "Random Trials",
+                "Single Trial",
+                st.session_state.acq_params["mouse"],
+            ),
         )
+
+        # if exp_type == "Single Trial":
 
 
 main()
